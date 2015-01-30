f6e1c46b9381cc3cd9e86b26a3c29dba1fea4f1c
ooz <oliverzscheyge@gmail.com>
2015-01-30 01:23:50 +0100

    Add version string, refactor some constants

--- source/AboutState.hx
+++ source/AboutState.hx
@@ -3,11 +3,17 @@ package;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.FlxG;
+import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 using flixel.util.FlxSpriteUtil;
 
 class AboutState extends FlxState
 {
+    public static inline var FONT_SIZE = 10;
+    public static inline var BACK_BUTTON_WIDTH = 128;
+    public static inline var BACK_BUTTON_HEIGHT = 32;
+
+    private var _versionNumber:FlxText;
     private var _creditsImg:FlxSprite;
     private var _btnCredits:FlxButton;
 
@@ -20,11 +26,23 @@ class AboutState extends FlxState
         _creditsImg.screenCenter();
         add(_creditsImg);
 
-        _btnCredits = new FlxButton(1024 - 128, 576 - 32, "", clickBack);
+        var appVersion = getVersionCode();
+        _versionNumber = new FlxText(0, 0, 0, "Duckstruction " + Std.string(appVersion), AboutState.FONT_SIZE);
+        _versionNumber.setBorderStyle(FlxText.BORDER_OUTLINE, 0xFF000000, 1.0);
+        add(_versionNumber);
+
+        _btnCredits = new FlxButton(1024 - AboutState.BACK_BUTTON_WIDTH, 
+                                    576 - AboutState.BACK_BUTTON_HEIGHT, "", clickBack);
         _btnCredits.loadGraphic("assets/images/backbutton_small.png");
         add(_btnCredits);
 
         super.create();
+    }
+
+    private function getVersionCode():String
+    {
+        // TODO: find a way to get the version code from the Project.xml
+        return "v0.0.1";
     }
 
     private function clickBack():Void
